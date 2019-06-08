7e7d22cc034fbb9efc6a89a175c7312df1ecc230
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-06-08 13:45:15 +0200

    Update

--- visualize-mnist.R
+++ visualize-mnist.R
@@ -1,8 +1,9 @@
 library(keras)
 library(shiny)
+library(ggrepel)
 library(tidyverse)
  
-# Load MNIST dataset Normalize and flatten 28x28 images using row-major layout (which is used by tensorflow).
+# Load MNIST dataset, normalize and flatten 28x28 images using row-major layout (which is used by tensorflow).
 # Also normalize the grayscale values into [0; 1] and one-hot-encode the labels.
 mnist   <- dataset_mnist()
 x.train <- array_reshape(mnist$train$x, c(nrow(mnist$train$x), 784)) / 255
@@ -10,8 +11,10 @@ x.test  <- array_reshape(mnist$test$x, c(nrow(mnist$test$x), 784)) / 255
 y.train <- to_categorical(mnist$train$y, 10)
 y.test  <- to_categorical(mnist$test$y, 10)
 
-# Returns ggplot-heatmap from a row-major 1D vector. Kept generic enough to plot arbitrary 2D heatmaps, since ggplot
-# does not have any heatmap geoms and 'image()' rotates the image for no healthy reason.
+# Return ggplot-heatmap from a row-major 1D vector.
+# 
+# Kept generic enough to plot arbitrary 2D heatmaps, since ggplot does not have any heatmap geoms and 'image()' rotates
+# the image for no healthy reason. Might be reused at some point.
 plot_heatmap <- function(data, nrow = 1, ncol = length(data)) {
 
   X = rep(1:ncol, times = nrow) # X = 123123123
@@ -26,41 +29,74 @@ plot_heatmap <- function(data, nrow = 1, ncol = length(data)) {
   return(heat.map)
 }
 
-# Define user interface
-ui <- fluidPage(
+# Return untrained Multi-Layer Perceptron Keras model with a single hidden layer.
+#
+# The number of hidden neurons is parameterized.
+initialize_model <- function(input.size = 784, hidden.units = 16, output.size = 10) {
+
+  # Defining is a 2-part process: Firstly, the layers are specified. Secondly, the loss-function and the optimization
+  # algorithm are chosed. The second step is referred to as 'compiling' the model in Keras lingo.
+  model <- keras_model_sequential(name = "MNIST Classifier")                                                   %>%
+    layer_dense(units = hidden.units, activation = 'relu', input_shape = c(input.size), name = "Hidden-Layer") %>%
+    layer_dense(units = output.size, activation = 'softmax', name = "Output-Layer")                            %>%
+    compile(loss = 'categorical_crossentropy', metrics = c('accuracy'), optimizer = optimizer_rmsprop())
+
+  return(model)
+}
+
+### Define the app's user interface
+ui <- fluidPage(title = "Live Neural Networks Demo", width="960px",
   fluidRow(
-    column(4, offset=2, align="center",
+    column(4, offset = 2, align="center",
       h2("Test Set"),
       sliderInput('test.index', label=NULL, min=1, max=10000, value=1000),
       strong(textOutput(outputId = 'label.test')),
       plotOutput(outputId = 'image.test')
     ),
-    column(4, align="center",
+    column(4, align = "center",
       h2("Training Set"),
       sliderInput('train.index', label=NULL, min=1, max=60000, value=8400),
       strong(textOutput(outputId = 'label.train')),
       plotOutput(outputId = 'image.train')
     )
+  ),
+  fluidRow(
+    column(4, align="center",
+      sliderInput("epoch.increment", label="Numer of Epochs to Train", value=16, min=1, max=64, step=1),
+      splitLayout(cellWidths=c("65%", "35%"),
+        actionButton("reset.button", label="Reset + Run", width="100%"),
+        actionButton("run.button", label="Run", width="100%")
+      ),
+      br(),
+      splitLayout(
+        h5("Hidden neurons: ", style="padding-bottom: -10px;"),
+        numericInput("hl.size", label=NULL, min=1, max=128, step=1, value=3)),
+      # TODO: Properly concatenate the static prefix and the dynamic counter value
+      h4(textOutput("epoch.counter")),
+      br(),
+      radioButtons("geom.plot.option", label=NULL, choices=list("Point", "Line"), inline=T),
+      checkboxInput("show.train.acc.option", label="Show training set history", value=T)
+    ),
+    column(8, align="center",
+      plotOutput("plot.history")
+    )
   )
 )
 
-# Define reactive outputs
+### Define the app's reactive outputs, i.e. the 'server'
 server <- function(input, output) {
 
   # Pixmap of test image
   output$image.test <- renderPlot({
 
-    img.out <- plot_heatmap(x.test[input$test.index,])
-
-    # information can be passed explicitly via a list
-    list(src = img.out)
-  }, width = 200, height = 200 )
+   return(plot_heatmap(x.test[input$test.index,], nrow = 28, ncol = 28))
+  })
   
   # Pixmap of training image
   output$image.train <- renderPlot({
 
-    plot_heatmap(x.train[input$train.index,])
-  }, width = 200, height = 200)
+    return(plot_heatmap(x.train[input$train.index,], nrow = 28, ncol = 28))
+  })
 
   # Label of test image
   output$label.test <- renderText({
